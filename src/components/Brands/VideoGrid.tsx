0320de4f4784a46b68fe866a769fa4960a2e9d52
Manmohanjit Singh <7972390+manmohanjit@users.noreply.github.com>
2025-03-25 00:32:30 +0800

    Remove text

--- src/components/Brands/VideoGrid.tsx
+++ src/components/Brands/VideoGrid.tsx
@@ -3,14 +3,13 @@ const VideoGrid = ({items, size = "lg", className = ""}) => (
     {items.map(({key, video, aspectRatio, text}) => (
       <div key={key}
            className={`max-w-[calc(50%-1.5rem)] ${size !== "xl" ? "md:max-w-[calc(25%-1.5rem)]" : "md:max-w-[calc(33%-1.5rem)]"}`}>
-        {video}
 
         <iframe loading="lazy" title="sk post 6"
                 src={video}
                 frameBorder="0"
                 className={`${aspectRatio || "aspect-[9/16]"} w-full h-auto`}
                 allow="fullscreen; picture-in-picture; clipboard-write; encrypted-media"></iframe>
-        
+
         {text && (
           <p className="mt-2 text-center">{text}</p>
         )}
